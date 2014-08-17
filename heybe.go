a01b9d3a8ff8609c61f42c62c74cf0f8b1b11710
turgay <turgaykivrak@gmail.com>
2014-08-17 03:39:06 +0300

    initial commit

---
+++ heybe.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	//	"./mustache"
+
+	"github.com/gorilla/mux"
+)
+
+var templates = template.Must(template.ParseGlob("./tmpl/*.html"))
+
+type ReadItem struct {
+	Name  string
+	Link  string
+	Descr string
+	Tag   string
+}
+
+type User struct {
+	UserName string
+	Password string
+	Email    string
+}
+
+var repository = Repository{}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, params interface{}) {
+	err := templates.ExecuteTemplate(w, tmpl, params)
+	//err := templates.ExecuteTemplate(w, tmpl+".html", params)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func listHandler(response http.ResponseWriter, request *http.Request) {
+	userName := getUserName(request)
+	if userName != "" {
+		items, err := repository.LoadItems()
+		fmt.Println(items, err)
+		if err != nil {
+			http.Redirect(response, request, "/list", http.StatusFound)
+			return
+		}
+
+		params := map[string]interface{}{"authuser": userName, "items": items}
+		renderTemplate(response, "list", params)
+	} else {
+		http.Redirect(response, request, "/", 302)
+	}
+}
+
+func aboutHandler(response http.ResponseWriter, request *http.Request) {
+	renderTemplate(response, "about", nil)
+
+}
+
+func registerHandler(response http.ResponseWriter, request *http.Request) {
+	if request.Method == "GET" {
+		renderTemplate(response, "register", nil)
+	} else {
+		userName := request.FormValue("name")
+		email := request.FormValue("email")
+		password := request.FormValue("password")
+
+		//TODO decode password
+		newUser := User{UserName: userName, Email: email, Password: password}
+		repository.AddUser(newUser)
+
+		http.Redirect(response, request, "/list", http.StatusFound)
+	}
+}
+
+func newItemHandler(response http.ResponseWriter, request *http.Request) {
+	if request.Method == "GET" {
+		renderTemplate(response, "add", nil)
+	} else {
+		name := request.FormValue("name")
+		link := request.FormValue("url")
+		descr := request.FormValue("descr")
+		tag := request.FormValue("tag")
+		ri := ReadItem{Name: name, Link: link, Descr: descr, Tag: tag}
+
+		repository.AddItem(ri)
+
+		http.Redirect(response, request, "/list", http.StatusFound)
+	}
+}
+
+//--------------
+
+// login handler
+
+func loginHandler(response http.ResponseWriter, request *http.Request) {
+	name := request.FormValue("userName")
+	pass := request.FormValue("password")
+	redirectTarget := "/"
+	fmt.Println("N=", name, " P=", pass)
+	fmt.Printf("%+v\n", request)
+	if name != "" && pass != "" {
+		// .. check credentials ..
+		setSession(name, response)
+		redirectTarget = "list"
+	}
+	http.Redirect(response, request, redirectTarget, 302)
+}
+
+// logout handler
+
+func logoutHandler(response http.ResponseWriter, request *http.Request) {
+	clearSession(response)
+	http.Redirect(response, request, "/", 302)
+}
+
+func indexPageHandler(response http.ResponseWriter, request *http.Request) {
+	renderTemplate(response, "login", nil)
+}
+
+// server main method
+
+var router = mux.NewRouter()
+
+func main() {
+	repository.InitReadItems()
+
+	router.HandleFunc("/", indexPageHandler)
+	router.HandleFunc("/list", listHandler)
+	http.HandleFunc("/add", newItemHandler)
+
+	router.HandleFunc("/login", loginHandler).Methods("POST")
+	router.HandleFunc("/logout", logoutHandler).Methods("POST")
+	router.HandleFunc("/about", aboutHandler)
+	router.HandleFunc("/register", registerHandler)
+
+	http.Handle("/", router)
+	http.ListenAndServe(":8000", nil)
+
+}
